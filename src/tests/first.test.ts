9c6f346987cb4365007e3a8e7d07b3bb4d7b37f9
MathMendesReis <matheusmendesreis@outlook.com>
2023-07-27 19:13:56 -0300

    testes adicionados

---
+++ src/tests/first.test.ts
@@ -0,0 +1,10 @@
+describe("Praticando com Jest", () => {
+
+    test("Primeiro teste", () => {
+            // lógica do primeiro teste
+    })
+
+    test("Segundo teste", () => {
+            // lógica do primeiro teste
+    })
+})
